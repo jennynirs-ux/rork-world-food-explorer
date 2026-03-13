44530f61faaba8d17109d4866a0e9aeabd11f7e6
jennynirs-ux <jenny.nirs@mojjo.se>
2026-03-13 15:55:07 +0100

    test(TEST-001): add Jest configuration for Expo

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|@tanstack/.*)',
+  ],
+  setupFilesAfterSetup: ['./jest.setup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: [
+    'lib/**/*.{ts,tsx}',
+    'components/**/*.{ts,tsx}',
+    'contexts/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.{ts,tsx}',
+    '**/*.test.{ts,tsx}',
+    '**/*.spec.{ts,tsx}',
+  ],
+};
